30670b4ddef9b3af11d1cb1e103fe155706793ee
jia <115862545+jjjyjyjyjj@users.noreply.github.com>
2026-04-12 20:04:57 -0400

    ugh

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['frontend-production-91482.up.railway.app'],
+  },
 })
